e9f5151d54995321b4077394073e84051414e604
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 19:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const statusele=document.getElementById("status");
+const enterbtn=document.getElementById("enterBtn");
+
+enterbtn.addEventListener('click',()=>{
+	statusele.innerText="Entered Metaverse";
+	const h1ele=document.createElement("h1");
+	h1ele.innerText=statusele.innerText;
+	statusele.parentNode.replaceChild(h1ele, statusele);
+});
